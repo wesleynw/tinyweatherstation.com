9d463bee5d68fc03b5eecc21313a0dd167e9cde5
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-07-05 14:47:27 -0700

    fixed sql query

--- getdata.php
+++ getdata.php
@@ -10,14 +10,16 @@ $connection = new MySQLi('178.128.152.62', 'webuser', '1234', 'weather');
 if($connection->connect_error){
 	die("Connection failed: " . $connection->connect_error);
 }
+echo "success";
 
 $query = "SELECT * FROM ". $_GET['loc'] . ";";
-$result = array($connection->query($query));
-// $result = array();
-while($data = $result->fetch_assoc()){
-	array_push($results, $data);
-}
-echo $result;
+echo $query;
+// $result = array($connection->query($query));
+// // $result = array();
+// while($data = $result->fetch_assoc()){
+// 	array_push($results, $data);
+// }
+// echo $result;
 
 
 
