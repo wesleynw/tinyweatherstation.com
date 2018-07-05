1672d07cfb7cf97f48cbf6bdce29ae1dd86f0d04
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-07-05 15:35:38 -0700

    substr

--- getdata.php
+++ getdata.php
@@ -21,7 +21,7 @@ while($row = $result->fetch_assoc()){
 
 switch ($_GET['id']) {
 	case 'temp_f':
-		// echo substr(($arrayResult[-1]-32)*0.5556, 0, 4);
+		echo substr(($arrayResult[-1]-32)*0.5556, 0, 4);
 		break;
 	case 'temp_c':
 		// echo $arrayResult[temperature][-1];
