6d466d31bfb81fc1127bbdc61cc76880568006bd
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-07-05 14:20:49 -0700

    changed getdata.php

--- getdata.php
+++ getdata.php
@@ -11,11 +11,7 @@ if($connection->connect_error){
 	die("Connection failed: " . $connection->connect_error);
 }
 
-<<<<<<< HEAD
 $query = "SELECT * FROM ", $_GET['loc'], ";";
-=======
-$query = "SELECT * FROM ", $_GET['loc'];
->>>>>>> 16f9378afb8843ae1ed5e2d4bf8766620711c17f
 $data = $connection->query($query)->fetch_assoc();
 echo $data;
 
@@ -26,7 +22,6 @@ echo $data;
 
 
 
-<<<<<<< HEAD
 // $sql = "SELECT * FROM home ORDER BY timestamp DESC LIMIT 1";
 // $sqlAll = "SELECT * FROM home";
 // $data = $connection->query($sql)->fetch_assoc();
@@ -58,38 +53,5 @@ echo $data;
 		// 			break;
 		//
 		// }
-=======
-$sql = "SELECT * FROM home ORDER BY timestamp DESC LIMIT 1";
-$sqlAll = "SELECT * FROM home";
-$data = $connection->query($sql)->fetch_assoc();
-$result = $connection->query($sqlAll);
-
-
-		switch($_GET['id']){
-			case "farenheit":
-				echo $data[temperature];
-				break;
-			case "celsius":
-				echo substr(($data[temperature]-32)*0.5556, 0, 4);
-				break;
-			case "humidity":
-				echo $data[humidity];
-				break;
-			case "pressure":
-				echo $data[pressure];
-				break;
-			case "time":
-				echo "Last Updated: " . Carbon::createFromFormat('Y-m-d H:i:s', $data[timestamp], 'UTC')->setTimezone('America/Los_Angeles')->diffForHumans();
-				break;
-			case "all":
-					$results = array();
-					while($data = $result->fetch_assoc()){
-						array_push($results, $data);
-					}
-					echo json_encode($results);
-					break;
-
-		}
->>>>>>> 16f9378afb8843ae1ed5e2d4bf8766620711c17f
 
 ?>
