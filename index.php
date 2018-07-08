d952ca627560f60bd94581afbdd4045c754b2a89
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-07-07 23:37:57 -0700

    fixing navbar

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 				<a class="brand-logo center" title="tinyweatherstation.com"><i class="material-icons">cloud</i></a>
 				<ul class="right">
 					<li>
-						<a class="" title="tinyweatherstation.com"><i class="material-icons">cloud</i></a>
+						<a class="" title="tinyweatherstation.com">Edit location <i class="material-icons">cloud</i></a>
 					</li>
 				</ul>
 			</div>
