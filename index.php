47fb035d30e0ac83dcc5778459fc2c55f1f38645
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-07-24 16:02:25 -0700

    modal fixed footer

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
 	</div>
 
 
-	<div id="tempGraphModal" class="modal" style="height: 100%">
+	<div id="tempGraphModal" class="modal modal-fixed-footer" style="height: 100%">
 		<div class="modal-content">
 			<div id="tempGraph">
 				<div class="progress">
