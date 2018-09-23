94faf85e645c41449c71a93b68d8280057befe4c
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2018-09-23 10:38:36 -0700

    consolidated arduino sketches into one

--- arduino_sketches/sensor_post/sensor_post.ino
+++ arduino_sketches/sensor_post/sensor_post.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
-//#include <Adafruit_Sensor.h>
-#include <Adafruit_MCP9808.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -8,7 +8,7 @@
 #include <MySQL_Cursor.h>
 
 //db credentials
-IPAddress server_addr(178,128,152,62); //ip of server
+IPAddress server_addr(1,1,1,1); //ip of server
 char user[] = "...";
 char password[] = "...";
 
@@ -16,44 +16,48 @@ char password[] = "...";
 char ssid[] = "...";
 char pass[] = "...";
 
-char INSERT_SQL[] = "INSERT INTO weather.home_indoor (`timestamp`, `temperature`) VALUES (CURRENT_TIMESTAMP, '%s');";
+char INSERT_SQL[] = "INSERT INTO \\\./// (`timestamp`, `temperature`, `humidity`, `pressure`) VALUES (CURRENT_TIMESTAMP, '%s', '%s', '%s');";
 char query[128];
 char temperature[10];
+char humidity[10];
+char pressure[10];
 
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
-Adafruit_MCP9808 mcp;
+Adafruit_BME280 bme;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  
+
   Serial.begin(115200);
   Serial.println("ESP8266 Online");
   int counter = 0;
-  
+
   //Start BME280 sensor
-  Serial.println("Looking for MCP9808 sensor...");
-  bool status = mcp.begin();
+  Serial.println("Looking for BME280 sensor");
+  bool status = bme.begin();
   if(!status){
     delay(500);
     Serial.print(".");
     counter++;
-    if(counter > 50){
+    if(counter > 10){
+      Serial.println('ESP Restarting')
       ESP.restart();
     }
   }
-  Serial.println("MCP9808 sensor connected");
+  Serial.println("BME280 sensor connected");
 
-  counter = 0;
   //Start WiFi
-  Serial.print("Connecting to WiFi...");
+  Serial.print("Connecting to WiFi");
+  counter = 0;
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED ) {
     delay (500);
     Serial.print (".");
     counter++;
-    if(counter > 50){
+    if(counter > 20){
+      Serial.println('ESP Restarting')
       ESP.restart();
     }
   }
@@ -64,20 +68,33 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   counter = 0;
-  Serial.print("Connecting to DB...");
+  Serial.print("Connecting to DB");
   while (conn.connect(server_addr, 3306, user, password) != true) {
     delay(500);
     Serial.print(".");
     counter++;
-    if(counter > 50){
+    if(counter > 15){
+      Serial.println('ESP Restarting')
       ESP.restart();
     }
   }
 
+  //Connect to db
+  counter = 0;
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  dtostrf(mcp.readTempC()*9.0/5.0 + 32, 5, 2, temperature);
-  sprintf(query, INSERT_SQL, temperature);
-  cur_mem->execute(query);
+  dtostrf(bme.readTemperature()*1.8+32, 5, 2, temperature);
+  dtostrf(bme.readHumidity(), 5, 2, humidity);
+  dtostrf(bme.readPressure()/1000, 5, 2, pressure);
+  sprintf(query, INSERT_SQL, temperature, humidity, pressure);
+
+  while(true) {
+    cur_mem->execute(query);
+    counter++;
+    if(counter > 15){
+      Serial.println('ESP Restarting')
+      ESP.restart();
+    }
+  }
   delete cur_mem;
   Serial.println("Data recorded");
   digitalWrite(LED_BUILTIN, HIGH);
